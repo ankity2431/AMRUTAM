b5cc2885d26b040c951de7a3eecb7c3f8255f6fc
ankity2431 <ankity2431@gmail.com>
2025-08-05 04:17:53 +0530

    path issue fixed

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', //  Hardcode this unless you're managing env vars properly
+  base: '/AMRUTAM/', //  Hardcode this unless you're managing env vars properly
   plugins: [react()],
 })
